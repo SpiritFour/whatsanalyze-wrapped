a39b0868791f39760f705eb15f9bdbadb032385f
athi <athi@netlight.com>
2025-11-09 22:44:35 +0100

    fixed padding

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="relative overflow-hidden pb-16">
+  <section class="relative overflow-hidden pb-0">
     <Polygon />
-    <div class="card container w-full h-full">
+    <div class="card card--flush container w-full h-full">
       <div v-if="heroCopy" class="flex flex-col gap-8">
         <div>
           <div class="flex flex-wrap items-center gap-4 text-sm text-gray-400 mb-4">
@@ -28,7 +28,7 @@
         </div>
         <div class="w-full">
           <Upload />
-          <p class="mt-3 text-sm text-center text-gray-500">
+          <p class="text-sm text-center text-gray-500">
             {{ $t("home.hero.usedBy") }}
           </p>
         </div>
